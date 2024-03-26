c67d8173754719a2ae8956b5176897c047623d17
Samar Abbas <samarabbas172003@gmail.com>
2024-03-27 03:55:44 +0530

    add styling

--- FRONTEND/src/components/updates.jsx
+++ FRONTEND/src/components/updates.jsx
@@ -1,11 +1,81 @@
 import React from "react";
-
-const updates = () => {
+import Navbar from "./Navbar";
+import Footern from "./footern";
+const Blog = () => {
   return (
-    <div>
-      <h1>Hello This is Updates</h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <section className="bg-white pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                <span className="mb-2 block text-lg font-semibold text-primary">
+                  Daily Updates{" "}
+                </span>
+                <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px]">
+                  Our Recent News
+                </h2>
+                <p className="text-base text-body-color dark:text-dark-6">
+                  There are many variations of passages of Lorem Ipsum available
+                  but the majority have suffered alteration in some form.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="-mx-4 flex flex-wrap">
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+          </div>
+        </div>
+      </section>
+
+      <Footern />
+    </>
   );
 };
 
-export default updates;
+export default Blog;
+
+const BlogCard = ({ date, CardTitle, CardDescription }) => {
+  return (
+    <>
+      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+        <div className="mb-10 w-full">
+          <div>
+            {date && (
+              <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose text-white">
+                {date}
+              </span>
+            )}
+            <h3>
+              <a
+                href="/#"
+                className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+              >
+                {CardTitle}
+              </a>
+            </h3>
+            <p className="text-base text-body-color dark:text-dark-6">
+              {CardDescription}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
