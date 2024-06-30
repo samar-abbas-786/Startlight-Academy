5d071046917b8827c415aa79916dd690f503dbae
Samar Abbas <samarabbas172003@gmail.com>
2024-07-01 02:27:22 +0530

    res3

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -9,11 +9,11 @@ export default function Navbar() {
     <nav className="bg-orange-900  text-white fixed w-full z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+          <div className="flex md:justify-evenly items-center">
             <div className="text-xl font-bold text-sky-50">
               📚 Starlight Academy
             </div>
-            <div className="hidden md:flex space-x-4 ml-10">
+            <div className="hidden md:flex   md:justify-evenly  space-x-4 ml-10">
               <Link to="/" className="hover:font-semibold">
                 HOME
               </Link>
